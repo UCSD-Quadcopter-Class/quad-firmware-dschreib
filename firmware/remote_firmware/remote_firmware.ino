3d5a6f8201dfff1941d90e43deafeb4e643af9be
danielshak <dshak@ucsd.edu>
2017-04-28 00:03:58 -0700

    Update code

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -3,7 +3,7 @@ int YAW = 0, yaw_val;
 int THROT = 1, throt_val;
 int ROLL = 2, roll_val;
 int PITCH = 3, pitch_val;
-
+char text[4];
 
 serLCD lcd;
 
@@ -14,27 +14,35 @@ void setup() {
 }
 
 void loop() {
+
+  //Printing gimbal values to lcd screen
   yaw_val = analogRead(YAW);
   throt_val = analogRead(THROT);
   roll_val = analogRead(ROLL);
   pitch_val = analogRead(PITCH);
-
+  
+  lcd.setCursor(1,8);
+  lcd.print("P:");
+  sprintf(text,"%4d",((pitch_val-115)*2));
+  lcd.print(text);
+  
   lcd.setCursor(0,0);
   lcd.print("Y:");
-  lcd.print((yaw_val-115)*2);
+  sprintf(text,"%4d",((yaw_val-115)*2));
+  lcd.print(text);
+
   lcd.setCursor(0,8);
   lcd.print("T:");
-  lcd.print((throt_val-115)*2);
+  sprintf(text,"%4d",(throt_val-115)*2);
+  lcd.print(text);
 
   lcd.setCursor(1,0);
   lcd.print("R:");
-  lcd.print((roll_val-115)*2);
-  lcd.setCursor(1,8);
-  lcd.print("P:");
-  lcd.print((pitch_val-115)*2);
-
+  sprintf(text,"%4d",(roll_val-115)*2);
+  lcd.print(text);
   
-  /* Printing to serial monitor
+  /*
+  //Printing  gimbal values to serial monitor
   Serial.print(" Yaw: ");
   Serial.print((yaw_val-115)*2);
   Serial.print(" Throttle: ");
@@ -42,7 +50,7 @@ void loop() {
   Serial.print(" Roll; ");
   Serial.print((roll_val-115)*2);
   Serial.print(" Pitch: ");
-  Serial.println((pitch_val-115)*2);
-  */
+  Serial.println((pitch_val-115)*2);*/
   
+  delay(10);
 }
