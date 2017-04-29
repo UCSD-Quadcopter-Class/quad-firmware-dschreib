cb7ed60b24000034d62728270267c6121afb2b36
danielshak <dshak@ucsd.edu>
2017-04-28 17:27:34 -0700

    code

--- firmware/quad_firmware/quad_firmware.ino
+++ firmware/quad_firmware/quad_firmware.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+int THROT = 1, throt_val;
+int PWM_THROT = 8, pwm;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  throt_val = analogRead(THROT);
 
+  if ((throt_val-115)*2 < 50) {
+    pwm = 0;
+  }
+  else if ((throt_val-115)*2 > 1420) {
+    pwm = 255;
+  }
+  else {
+    pwm = ((float)(throt_val-115)*2/1420)*255;
+  }
+
+  analogWrite(8,pwm);
+  Serial.println(pwm);
+  
+  delay(10);
 }
